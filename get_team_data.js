0294551edd0764f5586726dc54c9ed789c3ddd20
Yao, Raymond Yang <ryao49@gatech.edu>
2024-09-28 18:25:50 -0400

    Cleaned up code

--- get_team_data.js
+++ get_team_data.js
@@ -1,39 +1,29 @@
-
-const API_KEY = '4UGmkRNbDwYlMa7KOeCr4yFec1IjPMQfbUX0FOnsoU3FOfG2u0HZL0UcoWgLs0ao';
-const url = "https://www.thebluealliance.com/api/v3/event/2024gacmp/teams";
+const url = 'https://www.thebluealliance.com/api/v3/district/2024pch/teams';   
 const options = {
     headers: {
-        'X-TBA-Auth-Key': API_KEY
-    }};
-
-const output = 0;
-
-getData(url).then((result) => {
-    getTeamsData(result);
-    console.log(result);
-})
-.catch((error) => console.log(error));  
-
-async function getData(url) {
-    let x = await fetch(url, options);
-    let y = await x.json();
-    return y;
-}
-
-function getTeamsData(data) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i].nickname);
+        'X-TBA-Auth-Key': '4UGmkRNbDwYlMa7KOeCr4yFec1IjPMQfbUX0FOnsoU3FOfG2u0HZL0UcoWgLs0ao'
     }
-    var teams = "";
-    for (var i = 0; i < data.length - 1; i++) {
-        teams += data[i].nickname + ", ";
-    }
-    teams += data[data.length - 1].nickname;
+};
 
-    $(document).ready(function() {
-        $("button").click(function() {
-            $("p").text(teams);
-        });
+let teams;
+
+const getData = async () => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    teams = data;
+    return data;
+};
+
+(async () => {
+    await getData();
+    console.log(teams);
+})();
+
+$(document).ready(function(){
+    $("button").click(function(){
+        for (var i = 0; i < teams.length; ++i) {
+            $("p").append(teams[i].team_number + ", ");
+        }
     });
-}
+});
+    
