b4bf70f2df0dad97cfbf6ac06277af1556131ded
Mukil Sundaravadivel <mukilsundar@gmail.com>
2024-10-03 19:58:14 -0400

    change API call into Node.js script instead of running every time to get data

--- dev/get_team_data.mjs
+++ dev/get_team_data.mjs
@@ -1,4 +1,18 @@
-const eventUrl = 'https://www.thebluealliance.com/api/v3/district/2024pch/teams';   
+import { writeFileSync } from 'fs';
+
+let event = '2024gacar';
+let district = '2024pch';
+let isEvent = true; // true to get teams by event, false to sort teams by district
+let name;
+let url;
+
+if (isEvent) {
+    url = 'https://www.thebluealliance.com/api/v3/event/' + event + '/teams';
+    name = event;
+} else {
+    url = 'https://www.thebluealliance.com/api/v3/district/' + district + '/teams';
+    name = district;
+}
 const options = {
     headers: {
         'X-TBA-Auth-Key': '4UGmkRNbDwYlMa7KOeCr4yFec1IjPMQfbUX0FOnsoU3FOfG2u0HZL0UcoWgLs0ao'
@@ -18,13 +32,14 @@ const getData = async (url) => {
 let teamsData = new Array(); 
 const getTeamsData = async (data) => {
     for (var i = 0; i < teams.length; i++) {
-        teamUrl = 'https://www.thebluealliance.com/api/v3/team/frc' + teams[i];
+        let teamUrl = 'https://www.thebluealliance.com/api/v3/team/frc' + teams[i];
         let response = await fetch(teamUrl, options);
         let data = await response.json();
         teamsData.push(new Object());
         teamsData[i] = {
+            'team_number': teams[i],
             'city': data.city,
-            'country': data.country,
+            // 'country': data.country,
             'nickname': data.nickname,
             'rookie_year': data.rookie_year,
             'school_name': data.school_name
@@ -34,17 +49,15 @@ const getTeamsData = async (data) => {
 }
 
 (async () => {
-    await getData(eventUrl);
+    await getData(url);
     console.log(teams);
 
     await getTeamsData(teams);
-    console.log(teamsData);
-})();
+    console.log(teamsData);   
 
-$(document).ready(function(){
-    $("button").click(function(){
-        for (var i = 0; i < teams.length; ++i) {
-            $("p").append(teams[i] + ', ' + teamsData[i].nickname + '<br>');
-        }   
-    });
-});
+    writeFileSync(
+        '../data/' + name + '.json',
+        JSON.stringify({ teams: teamsData }, null, 2)
+    );
+
+})();
